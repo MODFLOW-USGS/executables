9cc50a8a1641cd0bfccd89f78cd8fdf8822d8751
jdhughes-usgs <jdhughes@usgs.gov>
2022-11-18 14:17:36 -0600

    ci: update CI to build for all OSs with Intel (#9)

--- .github/common/build_executables.py
+++ .github/common/build_executables.py
@@ -28,13 +28,14 @@ def get_cctag() -> str:
 
 
 if __name__ == "__main__":
-    path = (
-        pl.Path(os.path.dirname(pymake.__file__)) / "../"
-    ).resolve() / "examples/buildall.py"
+    path = (pl.Path(os.path.dirname(pymake.__file__)) / "../").resolve()
+    print(f"path to pymake: {path}")
+
+    file_path = pl.Path(".github/common/buildall.py")
 
     cmds = [
         "python",
-        path,
+        file_path,
         f"--appdir={get_ostag()}",
         "-fc=ifort",
         f"-cc={get_cctag()}",
@@ -43,8 +44,13 @@ if __name__ == "__main__":
     ]
 
     retries = 3
+    success = False
     for idx in range(retries):
         p = subprocess.run(cmds)
         if p.returncode == 0:
+            success = True
             break
         print(f"run {idx + 1}/{retries} failed...rerunning")
+
+    if not success:
+        raise RuntimeError("could not build the executables")
--- .github/common/install-python.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python -m pip install --upgrade pip
-pip install wheel
-pip install https://github.com/modflowpy/pymake/zipball/develop
-
