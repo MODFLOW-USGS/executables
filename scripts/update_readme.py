ba7ac75eda00fabbb6105b82c6687ddfc820c50e
w-bonelli <wesbonelli@gmail.com>
2023-02-26 08:12:32 -0500

    ci: fix paths in update_readme.py, use GITHUB_TOKEN to bypass rate limits

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -1,16 +1,17 @@
 import pathlib as pl
 
+proj_root = pl.Path(__file__).parent.parent
 target_file = pl.Path("code.md")
 TAG = "| Program | Version | UTC Date |"
 FILES = ("code.json", "code.md")
 
 
 def _update_readme() -> None:
-    with open("../README.md", "r") as f:
+    with open(proj_root / "README.md", "r") as f:
         readme_md = f.read().splitlines()
     with open("code.md", "r") as f:
         code_md = f.read().splitlines()
-    with open("../README.md", "w") as f:
+    with open(proj_root / "README.md", "w") as f:
         for line in readme_md:
             if TAG not in line:
                 f.write(f"{line}\n")
